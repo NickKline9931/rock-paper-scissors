2b14fd117484de1819518de168ecf92e106c6ef8
Nick <nickkline9931@gmail.com>
2022-04-14 11:44:05 -0400

    Disable buttons when game completes

--- script.js
+++ script.js
@@ -19,72 +19,104 @@ const tie = ('Tie!');
     
 function playRound() {
     let cSelect = cPick;
-    let pSelect = prompt('Rock Paper Scissors!');
+    let pSelect = pChoice;
     if (pSelect.toLowerCase() == cSelect().toLowerCase()) {
-    console.log(++tieCount);
+    alert(tie);
+    ++tieCount;
     return tie;
     } else if ((pSelect.toLowerCase() == 'rock' && cSelect() == 'Scissors') 
     || (pSelect.toLowerCase() == 'paper' && cSelect() == 'Rock') 
     || (pSelect.toLowerCase() == 'scissors' && cSelect() == 'Paper')) {
-    console.log(++pScore);
+    alert(win);
+    ++pScore;
     return win;
     } else {
-    console.log(++cScore);
+    alert(lose);
+    ++cScore;
     return lose;
 }
 }
-
-function game() {
-const winner = ('Winner!');
-const loser = ('Loser!');
-const draw = ('Draw!');
-if (pScore > cScore) {
-console.log(winner);
-return winner;
-}else if (pScore < cScore) {
-console.log(loser);
-return loser; 
-}else {
-console.log(draw);
-return draw;
-}
-}
-
-alert(game());
 
 const body = document.getElementsByTagName('body');
 
 const select = document.createElement('div');
 document.body.appendChild(select);
 
+let pChoice;
+
 const rock = document.createElement('button');
 rock.textContent = "Rock";
-rock.addEventListener("click", playRound);
+rock.addEventListener("click", updateScore); {
+    pChoice = 'rock';
+}
+
 
 select.appendChild(rock);
 
 const paper = document.createElement('button');
 paper.textContent = "paper";
-paper.addEventListener("click", playRound);
+paper.addEventListener("click", updateScore); {
+    pChoice = 'paper';
+}
 
 select.appendChild(paper);
 
 const scissors = document.createElement('button');
 scissors.textContent = "scissors";
-scissors.addEventListener("click", playRound);
-
+scissors.addEventListener("click", updateScore); {
+    pChoice = 'scissors';
+}
 select.appendChild(scissors);
 
+player.textContent = "Player Score: 0";
+com.textContent = "Computer Score: 0";
+ties.textContent = "Ties: 0";
+
+function updateScore() {
+    let player = document.getElementById("player");
+    let com = document.getElementById("com");
+    let ties = document.getElementById("ties");
+    playRound();
+    player.textContent = `Player Score: ${pScore}`;
+    com.textContent = `Computer Score: ${cScore}`;
+    ties.textContent = `Ties: ${tieCount}`;
+    endMessage();
+    endGame();
+}
+
+function scoreCap() {
+    if (pScore == 5 || cScore == 5){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const winner = 'Winner!'
+const loser = 'Loser!'
+
+function endMessage() {
+if (pScore == 5){
+    alert(winner);
+    return true;
+}else if (cScore == 5) {
+    alert(loser);
+    return true;
+} else {
+    return false;
+}
+}
+
+function endGame() {
+    if (pScore == 5 || cScore == 5){
+        rock.removeEventListener('click', updateScore);
+        paper.removeEventListener('click', updateScore);
+        scissors.removeEventListener('click', updateScore);
+    }
+}
 
 
 
 
 
-  
-     
     
-
-
-
-
-
